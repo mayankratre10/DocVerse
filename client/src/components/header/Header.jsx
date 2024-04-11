cdc1123459ade04a4d55ea87e105e94335134141
mayank kumar ratre <mayankratre10@gmail.com>
2024-04-11 17:24:32 +0530

    updated

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -5,7 +5,10 @@ import userIcon from '../../assets/userIcon.png'
 function Header({user,logOut}) {
   return (
     <div className='header'>
+        <div>
+        <p className='createdBy'>Created By Mayank Kumar Ratre</p>
         <img src={logo} alt="N/A" />
+        </div>
         <div className='user'>
         <img className='userIcon' src={userIcon} alt="N/A" />
         <p>{user.userName}</p>
